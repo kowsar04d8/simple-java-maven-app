8569c1bbdfd8feb25cad188239d566b36296839d
kowsar04d8 <103165281+kowsar04d8@users.noreply.github.com>
2023-10-13 15:30:30 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -34,9 +34,9 @@ public class AppTest
     {
         App.main(null);
         try {
-            assertEquals("Hello KOWSAR!" + System.getProperty("line.separator"), outContent.toString());
+            assertEquals("Hello KOWSAR how are u?! " + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello KOWSAR!\"");
+            fail("\"message\" is not \"Hello KOWSAR how are u?!\"");
         }
     }
 
