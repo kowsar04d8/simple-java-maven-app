5856ea04aa24ae8dc71169c7813fd7b294804529
kowsar04d8 <103165281+kowsar04d8@users.noreply.github.com>
2023-11-01 15:22:32 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -36,7 +36,7 @@ public class AppTest
         try {
             assertEquals("Hello" + System.getProperty("line.separator"), outContent.toString());
         } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello"");
+            fail("\"message\" is not \"Hello\"");
         }
     }
 
